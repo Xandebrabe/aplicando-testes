5b16f4040c877e0d4da0f58585f05186ad31481e
Xandebrabe <alexandre.souza@sou.inteli.edu.br>
2024-05-27 01:54:39 -0300

    feat: add xunit test

--- xUnitTests/UnitTest1.cs
+++ xUnitTests/UnitTest1.cs
@@ -1,10 +1,24 @@
-namespace xUnitTests;
+using System;
+using Temperatura;
+using Xunit;
 
-public class UnitTest1
+namespace xUnitTests
 {
-    [Fact]
-    public void Test1()
+    public class TestesConversorTemperatura
     {
-
+        [Theory]
+        [InlineData(32, 0)]
+        [InlineData(47, 8.33)]
+        [InlineData(86, 30)]
+        [InlineData(90.5, 32.5)]
+        [InlineData(120.18, 48.99)]
+        [InlineData(212, 100)]
+        public void TestarConversaoTemperatura(
+            double fahrenheit, double celsius)
+        {
+            double valorCalculado =
+                ConversorTemperatura.FahrenheitParaCelsius(fahrenheit);
+            Assert.Equal(celsius, valorCalculado);
+        }
     }
 }
